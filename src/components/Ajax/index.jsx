025eeaa62a3fb696507135409639e91f4ac626eb
Sun Lin <40912737+sunlin92@users.noreply.github.com>
2019-05-08 01:40:15 +0800

    Update index.jsx

--- src/components/Ajax/index.jsx
+++ src/components/Ajax/index.jsx
@@ -2,35 +2,25 @@ import axios from 'axios'
 import { message } from 'antd';
 export default class Ajax {
     static ajax = (
-        method,
-        url,
-        data,
-        baseURL,
-        timeout = 5000,
-    ) => (
-            new Promise(
-                (resolve, reject) => {
-                    axios(
-                        method === 'get' ?
-                            {
-                                method,
-                                url,
-                                params: data,
-                                baseURL,
-                                timeout,
-                            }
-                            :
-                            {
-                                method,
-                                url,
-                                data,
-                                baseURL,
-                                timeout,
-                            }
-                    ).then(response => resolve(response.data)
-                    // response 是http请求的响应 response.data 可以拿到业务层的数据
-                    ).catch(() => message.error("请求发送失败"))
-                    })
-            )
-        )
+	method,
+	url,
+	data,
+	baseURL,
+	timeout = 5000,
+	) => new Promise((resolve, reject) => axios(
+			method === 'get'? {
+				method,
+				url,
+                params: data,
+				baseURL,
+				timeout,
+			}: {
+				method,
+				url,
+				data,
+				baseURL,
+				timeout,
+				}
+				// response 是http请求的响应 response.data 可以拿到业务层的数据
+				).then(response => resolve(response.data)).catch(() => message.error("请求发送失败")))
 }
